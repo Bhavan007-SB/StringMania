5605bf7617d704c03e4d07c8f709fe3d987737f1
Bhavan <bhavan.sb2001@gmail.com>
2023-02-17 13:22:55 +0000

    StringMania
    created

---
+++ src/main/java/utility/StringMethod.java
@@ -0,0 +1,33 @@
+package utility;
+
+public class StringMethod {
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public String lower(String a) {
+        return a.toLowerCase();
+    }
+
+    public String upper(String a) {
+        return a.toUpperCase();
+    }
+
+    public String firstCharacter(String a) {
+        return String.valueOf(a.charAt(0));
+    }
+
+    public String length(String a) {
+        return String.valueOf(a.length());
+    }
+
+    public String trim(String a) {
+        return a.trim();
+    }
+}
